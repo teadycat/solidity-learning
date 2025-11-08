4108fc8188e37d41f80c171d881edaa9efc23503
teadycat <dori4beardori@naver.com>
2025-11-09 01:41:55 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,13 +9,13 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
         // transaction
         // from, to, data, value, gas, ...
-        _mint(1*10**uint256(decimals), msg.sender); // 1 MT
+        _mint(_amount*10**uint256(decimals), msg.sender); // 1 MT
     }
 
     function _mint(uint256 amount, address owner) internal {
@@ -23,6 +23,12 @@ contract MyToken {
         // balanceOf[owner] = balanceOf[owner] + amount;
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer (uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance"); // 
+        balanceOf[msg.sender]-= amount;
+        balanceOf[to] += amount;
     }
 
     // function totalSupply() external view returns (uint256) {
