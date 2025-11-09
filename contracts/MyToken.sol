9cbe8c7c6dd85ba4b6cff083cda20eecf25476cd
teadycat <dori4beardori@naver.com>
2025-11-09 18:56:21 +0900

    add mint

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -36,6 +36,10 @@ contract MyToken {
         emit Transfer(from, to, amount);
     }
 
+    function mint(uint256 amount, address owner) external {
+        _mint(amount, owner);
+    }
+
     function _mint(uint256 amount, address owner) internal {
 
         // totalSupply = totalSupply + amount;
